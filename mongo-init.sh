5955a11f21ee245456668840b7f9ec04a0836e33
gierr7976 <gierr7976@gmail.com>
2023-09-12 16:40:38 +0300

    feat: MongoDB configuration

---
+++ mongo-init.sh
@@ -0,0 +1,16 @@
+set -e
+
+mongo << EOF
+db = db.getSiblingDB('admin');
+
+db.auth('$MONGO_INITDB_ROOT_USERNAME', '$MONGO_INITDB_ROOT_PASSWORD');
+
+db.createUser({
+  user: '$MONGO_ADMIN',
+  pwd: '$MONGO_ADMIN_PWD',
+  roles: [
+    { role: "userAdminAnyDatabase", db: "admin" },
+    { role: "readWriteAnyDatabase", db: "admin" }
+  ]
+})
+EOF
