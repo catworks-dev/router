89ea553294867dfec5b623bf422ab485957dd61f
gierr7976 <gierr7976@gmail.com>
2023-09-12 17:50:37 +0300

    fix: Mongo init config

--- mongo-init.sh
+++ mongo-init.sh
@@ -1,13 +1,13 @@
 set -e
 
 mongo << EOF
-db = db.getSiblingDB('admin');
+db = db.getSiblingDB("admin");
 
-db.auth('$MONGO_INITDB_ROOT_USERNAME', '$MONGO_INITDB_ROOT_PASSWORD');
+db.auth("$MONGO_INITDB_ROOT_USERNAME", "$MONGO_INITDB_ROOT_PASSWORD")
 
 db.createUser({
-  user: '$MONGO_ADMIN',
-  pwd: '$MONGO_ADMIN_PWD',
+  user: "$MONGO_ADMIN",
+  pwd: "$MONGO_ADMIN_PWD",
   roles: [
     { role: "userAdminAnyDatabase", db: "admin" },
     { role: "readWriteAnyDatabase", db: "admin" }
